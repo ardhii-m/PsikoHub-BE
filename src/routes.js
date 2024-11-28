164b01e133b21c3ed1a953551654ec212e7ad96f
ardhii-m <ardhiim008@gmail.com>
2024-11-28 16:53:50 +0700

    add basic routing

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,19 @@
 const routes = [
-
+  {
+    method: 'POST',
+    path: '/booking_order',
+    handler: () => {},
+  },
+  {
+    method: 'GET',
+    path: '/doctors',
+    handler: () => {},
+  },
+  {
+    method: 'GET',
+    path: '/doctors/{doctor_id}',
+    handler: () => {},
+  },
 ];
 
 module.exports = routes;
